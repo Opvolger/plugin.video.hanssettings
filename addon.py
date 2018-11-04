044e95277c726bd330342d9ec04b03ddd0e362c6
Bas Magré <Opvolger@gmail.com>
2018-11-04 20:11:00 +0100

    xbmcswift2 verwijderd, is iet up2date en gaf errors.
    nu default example van kodi aangehouden, werkt veel beter.

--- addon.py
+++ addon.py
@@ -6,17 +6,19 @@
     :license: GPLv3, see LICENSE.txt for more details.
     
 '''
-from xbmcswift2 import Plugin, SortMethod
+import sys
+from urllib import urlencode
+from urlparse import parse_qsl
 import resources.lib.hanssettings
 import time
 import xbmcplugin, xbmcgui
 
 PLUGIN_NAME = 'hanssettings'
 PLUGIN_ID = 'plugin.video.hanssettings'
-plugin = Plugin(PLUGIN_NAME, PLUGIN_ID, __file__)
 
 hanssettings = resources.lib.hanssettings.HansSettings()
-addon_handle = int(sys.argv[1])
+_url = sys.argv[0]
+_handle = int(sys.argv[1])
 
 try:
     import StorageServer
@@ -26,45 +28,157 @@ except:
 cache = StorageServer.StorageServer(PLUGIN_ID, 10) # (Your plugin name, Cache time in hours)
 buffer = 'leeg'
 
+def get_url(**kwargs):
+    """
+    Create a URL for calling the plugin recursively from the given set of keyword arguments.
+    :param kwargs: "argument=value" pairs
+    :type kwargs: dict
+    :return: plugin call URL
+    :rtype: str
+    """
+    return '{0}?{1}'.format(_url, urlencode(kwargs))
 
-@plugin.route('/')
-def index():
-    ##main, alle shows
-    items = [{
-        'path': plugin.url_for('show_streams', filename=item['filename']),
-        'label': item['label']
-    } for item in cache.cacheFunction(hanssettings.get_overzicht)]    
-    return plugin.finish(items)
+def get_categories():
+    streamfiles = list()
+    streams = cache.cacheFunction(hanssettings.get_overzicht)
+    for stream in streams:
+        streamfiles.append(stream['label'])
+    return streamfiles
 
-@plugin.route('/streams/<filename>/')
-def show_streams(filename):
-    return show_items(cache.cacheFunction(hanssettings.get_items,filename), filename)
+def get_videos(streamfile):
+    streams = cache.cacheFunction(hanssettings.get_items,streamfile)
+    return streams
 
-@plugin.route('/lectures/<fileandstream>')
-def play_lecture(fileandstream):
-    filename,stream = fileandstream.split('-:-:-')
-    items = cache.cacheFunction(hanssettings.get_items,filename)
-    for item in items:
-        if (item['stream'] == stream):
-            listitem = xbmcgui.ListItem(item['label'])
-            listitem.setInfo('video', {'Title': item['label']})
-            xbmc.Player().play(stream, listitem)
+def list_categories():
+    """
+    Create the list of video categories in the Kodi interface.
+    """
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_handle, 'Hans Settings Streams')
+    # Set plugin content. It allows Kodi to select appropriate views
+    # for this type of content.
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get video categories
+    categories = get_categories()
+    # Iterate through categories
+    for category in categories:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=category)
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        # list_item.setArt({'thumb': VIDEOS[category][0]['thumb'],
+        #                   'icon': VIDEOS[category][0]['thumb'],
+        #                   'fanart': VIDEOS[category][0]['thumb']})
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
+        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': category,
+                                    # 'genre': category,
+                                    'mediatype': 'video'})
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=listing&category=Animals
+        url = get_url(action='listing', category=category)
+        # is_folder = True means that this item opens a sub-list of lower level items.
+        is_folder = True
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
 
-def show_items(opgehaaldeitemsclass, filename):
-    items = list()
-    for item in opgehaaldeitemsclass:
-        path = __getpath(item, filename)
-        xbmc.log(path, xbmc.LOGNOTICE)
-        items.append({
-            'path': path,
-            'label': item['label'],
-            'is_playable': True
-            })
-    return plugin.finish(items,sort_methods=[SortMethod.LABEL])
+def list_videos(category):
+    """
+    Create the list of playable videos in the Kodi interface.
+    :param category: Category name
+    :type category: str
+    """
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_handle, category)
+    # Set plugin content. It allows Kodi to select appropriate views
+    # for this type of content.
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get the list of videos in the category.
+    videos = get_videos(category)
+    # Iterate through videos.
+    for video in videos:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=video['label'])
+        # Set additional info for the list item.
+        # 'mediatype' is needed for skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': video['label'],
+                                    # 'genre': video['genre'],
+                                    'mediatype': 'video'})
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        # list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
+        # Set 'IsPlayable' property to 'true'.
+        # This is mandatory for playable items!
+        list_item.setProperty('IsPlayable', 'true')
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
+        url = get_url(action='play', video=video['stream'])
+        # Add the list item to a virtual Kodi folder.
+        # is_folder = False means that this item won't open any sub-list.
+        is_folder = False
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
 
-def __getpath(item, filename):
-    if (item['stream'].find('?#User-Agent') > -1):
-        return item['stream'].partition('?#User-Agent')[0] + '|Referer='+item['stream'].replace('?#User-Agent','&User-Agent')
-    return plugin.url_for('play_lecture', fileandstream=filename + '-:-:-' + item['stream'])
+def play_video(path):
+    """
+    Play a video by the provided path.
+    :param path: Fully-qualified video URL
+    :type path: str
+    """
+    # Create a playable item with a path to play.
+    if (path.find('?#User-Agent') > -1):
+        path = path.partition('?#User-Agent')[0] + '|User-Agent'+path.partition('?#User-Agent')[2]
+    xbmc.log('path: ' + path, xbmc.LOGNOTICE)
+    play_item = xbmcgui.ListItem(path=path)
+    # Pass the item to the Kodi player.
+    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+
+def router(paramstring):
+    """
+    Router function that calls other functions
+    depending on the provided paramstring
+    :param paramstring: URL encoded plugin paramstring
+    :type paramstring: str
+    """
+    # Parse a URL-encoded paramstring to the dictionary of
+    # {<parameter>: <value>} elements
+    params = dict(parse_qsl(paramstring))
+    # Check the parameters passed to the plugin
+    if params:
+        if params['action'] == 'listing':
+            # Display the list of videos in a provided category.
+            list_videos(params['category'])
+        elif params['action'] == 'play':
+            # Play a video from a provided URL.
+            play_video(params['video'])
+        else:
+            # If the provided paramstring does not contain a supported action
+            # we raise an exception. This helps to catch coding errors,
+            # e.g. typos in action names.
+            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+    else:
+        # If the plugin is called from Kodi UI without any parameters,
+        # display the list of video categories
+        list_categories()
+
+
 if __name__ == '__main__':
-    plugin.run()
+    # Call the router function and pass the plugin call parameters to it.
+    # We use string slicing to trim the leading '?' from the plugin call paramstring
+    router(sys.argv[2][1:])

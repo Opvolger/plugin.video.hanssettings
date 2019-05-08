99853de6732858962ed30b73dcae6bd4d61bc64d
Opvolger <Opvolger@gmail.com>
2019-05-08 16:39:22 +0200

    wat code netter gemaakt en opgeruimd.

--- addon.py
+++ addon.py
@@ -53,45 +53,70 @@ def get_url(**kwargs):
     return '{0}?{1}'.format(_url, urlencode(kwargs))
 
 
-def get_categories(content_type):
-    soort = get_soort(content_type)
-    githubfiles = _cache.cacheFunction(_hanssettings.get_dataoverzicht, soort)
-    return _hanssettings.get_overzicht(githubfiles, soort)
+def get_github_streamfilenames_list(content_type):
+    # cache bouquet-github-file
+    stream_list_github = _cache.cacheFunction(
+        _hanssettings.get_data_from_github_file_bouquets, content_type)
+    return _hanssettings.get_stream_files_from_bouguet(stream_list_github, content_type)
 
 
-def get_items(streamfile):
+def get_items(github_streamfile):
+    # cache github-file
     streamsdatafile = _cache.cacheFunction(
-        _hanssettings.get_datafromfilegithub, streamfile)
+        _hanssettings.get_data_from_github_file, github_streamfile)
     return _hanssettings.get_items(streamsdatafile)
 
 
 def list_categories(content_type):
     xbmcplugin.setPluginCategory(_handle, _addon.getLocalizedString(32004))
     xbmcplugin.setContent(_handle, 'files')
-    i = 0
-    categories = get_categories(content_type)
-    categoriesLength = len(categories)
+
+    # ophalen alle bestandsnamen welke we kunnen ophalen in github.
+    github_stream_filenames = get_github_streamfilenames_list(content_type)
+
+    # totaal aantal streambestanden welke zijn op te halen van github.
+    count_stream_filenames = len(github_stream_filenames)
+
+    # setup progress bar
     progress = xbmcgui.DialogProgress()
     progress.create(_addon.getLocalizedString(32002),
                     _addon.getLocalizedString(32001))
-    for category in categories:
+
+    # init teller voor progress bar
+    i = 0
+    for filename in github_stream_filenames:
         i = i + 1
-        progressText = _addon.getLocalizedString(32003) % (i, categoriesLength)
+
+        # tekst prgress-bar, met met netjes voorgang er in met:  "files: x/y gehad"
+        progressText = _addon.getLocalizedString(
+            32003) % (i, count_stream_filenames)
+
         # python2 int delen door int = altijd een int, dus eerst omzetten naar een float
-        progress.update(int(100 / float(categoriesLength) * i),
+        # percentage bepaling
+        progress.update(int(100 / float(count_stream_filenames) * i),
                         "", progressText)
         if progress.iscanceled():
+            # als iemand de progress-bar stopt, dan alleen laten zien wat je al hebt, spring uit deze loop.
             break
+
+        # we gaan file voor file ophalen in deze loop
         datafile = _cache.cacheFunction(
-            _hanssettings.get_datafromfilegithub, category)
-        list_item = xbmcgui.ListItem(
-            label=_hanssettings.get_name(datafile, category))
-        list_item.setInfo(get_context(content_type), {'title': _hanssettings.get_name(datafile, category),
+            _hanssettings.get_data_from_github_file, filename)
+
+        # na de file ophalen gaan we hem in een item stoppen
+        name = _hanssettings.get_name(datafile, filename)
+        list_item = xbmcgui.ListItem(label=name)
+        list_item.setInfo(get_context(content_type), {'title': name,
                                                       'mediatype': get_context(content_type)})
-        url = get_url(action='listing', category=category,
+
+        # url voor navigeren.
+        url = get_url(action='listing', filename=filename,
                       content_type=content_type)
+
+        # alles in deze loop in een folder
         is_folder = True
         xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # we hebben alles geladen, de progress bar kan gesloten worden
     progress.close()
     del progress
     xbmcplugin.endOfDirectory(_handle)
@@ -108,39 +133,48 @@ def add_playable_listitem(item, content_type):
     xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
 
 
-def list_items_and_subfolder(category, content_type):
+def list_items_and_subfolder(filename, counter, content_type):
     datafile = _cache.cacheFunction(
-        _hanssettings.get_datafromfilegithub, category)
-    xbmcplugin.setPluginCategory(
-        _handle, _hanssettings.get_name(datafile, category))
+        _hanssettings.get_data_from_github_file, filename)
+    category_name = ''
+    itemlist = list()    
+    if (counter == None):
+        # we openen een hoofdfile, dus alle items
+        itemlist = get_items(filename)
+        # naam van github-file overnemen
+        category_name = _hanssettings.get_name(datafile, filename)
+    else:
+        # we openen een sub-folder, dus alleen deze item toevoegen
+        item = _hanssettings.get_items_subfolder(datafile, counter)
+        # nu even niet als een subfolder gedragen, we willen nu de stream hier van hebben
+        item['subfolder'] = False
+        itemlist.append(item)
+        # naam overnemen van sub-folder
+        category_name = item['label']
     xbmcplugin.setContent(_handle, 'files')
-    for item in get_items(category):
+    xbmcplugin.setPluginCategory(_handle, category_name)
+
+    # langs gaan van alle items
+    for item in itemlist:
         if (item['subfolder']):
+            # we hebben een subfolder te pakken.
+            # deze zijn allemaal genummerd.
             list_item = xbmcgui.ListItem(label=item['label'])
             list_item.setInfo(get_context(content_type), {
                               'title': item['label'], 'mediatype': get_context(content_type)})
-            url = get_url(action='subfolder', category=category,
+            url = get_url(action='subfolder', filename=filename,
                           counter=item['counter'], content_type=content_type)
             is_folder = True
             xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
         else:
+            # we hebben een directe stream te pakken, geef deze weer.
             for stream in item['streams']:
                 add_playable_listitem(stream, content_type)
     xbmcplugin.endOfDirectory(_handle)
 
 
-def list_subfolder(category, counter, content_type):
-    datafile = _cache.cacheFunction(
-        _hanssettings.get_datafromfilegithub, category)
-    item = _hanssettings.get_items_subfolder(datafile, counter)
-    xbmcplugin.setPluginCategory(_handle, item['label'])
-    xbmcplugin.setContent(_handle, 'files')
-    for stream in item['streams']:
-        add_playable_listitem(stream, content_type)
-    xbmcplugin.endOfDirectory(_handle)
-
-
 def play_item(path):
+    # als er een user-agent moet worden meegeven, dit moet iets anders onder kodi.
     if (path.find('?#User-Agent') > -1):
         path = path.partition(
             '?#User-Agent')[0] + '|User-Agent'+path.partition('?#User-Agent')[2]
@@ -151,13 +185,9 @@ def play_item(path):
 def get_context(content_type):
     if content_type == 'audio':
         return 'music'
+    if content_type == 'music':
+        return 'music'
     return 'video'
-
-
-def get_soort(content_type):
-    if content_type == 'audio':
-        return 'radio'
-    return 'tv'
 
 
 def router(paramstring):
@@ -172,16 +202,21 @@ def router(paramstring):
     args = parse_qs(paramstring)
     # Check the parameters passed to the plugin
     content_type = args.get('content_type', ['video'])[0]
-    xbmc.log('content_type: ' + content_type, xbmc.LOGNOTICE)
+    # xbmc.log('content_type: ' + content_type, xbmc.LOGNOTICE)
     action = args.get('action', ['list'])[0]
     if action == 'listing':
-        list_items_and_subfolder(args.get('category', None)[0], content_type)
+        # we openen een filestream
+        list_items_and_subfolder(args.get('filename', None)[
+                                 0], None, content_type)
     elif action == 'subfolder':
-        list_subfolder(args.get('category', None)[0],
-                       args.get('counter', None)[0], content_type)
+        # we openen een sub-sectie van een file stream
+        list_items_and_subfolder(args.get('filename', None)[
+                                 0], args.get('counter', None)[0], content_type)
     elif action == 'play':
+        # we gaan afspelen
         play_item(args.get('item', None)[0])
     else:
+        # standaard gaan we naar de hoofdpagina met alle filestreams uit de bouquet-file van het juiste type (audio/video)
         list_categories(content_type)
 
 
--- resources/lib/hanssettings.py
+++ resources/lib/hanssettings.py
@@ -35,69 +35,91 @@ class HansSettings:
     def __init__(self):
         self.PY3 = sys.version_info[0] == 3
 
-    def get_datafromfilegithub(self, file):
+    @staticmethod
+    def get_soort(content_type):
+        if content_type == 'audio':
+            return 'radio'
+        return 'tv'
+
+    def get_data_from_github_file(self, github_file):
         url = 'https://raw.githubusercontent.com/haroo/HansSettings/master/e2_hanssettings_kabelNL/'
-        req = Request(url + file)
+        req = Request(url + github_file)
         req.add_header(
             'User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0')
         req.add_header('Content-Type', 'text/html; charset=utf-8')
         response = urlopen(req)
-        linkdata = response.read()
+        filedata = response.read()
         if (self.PY3):
-            linkdata = linkdata.decode('utf-8', 'backslashreplace')
+            filedata = filedata.decode('utf-8', 'backslashreplace')
         response.close()
-        return linkdata
+        return filedata
 
-    def get_dataoverzicht(self, soort):
-        return self.get_datafromfilegithub('bouquets.' + soort)
+    def get_data_from_github_file_bouquets(self, content_type):
+        # ophalen van bouquet met daarin alle stream files.
+        return self.get_data_from_github_file('bouquets.' + self.get_soort(content_type))
 
-    def get_overzicht(self, linkdata, soort):
+    def get_stream_files_from_bouguet(self, filedata, content_type):
+        # het uitlezen van de githubfiles met streams erin (deze staan in de bouquets.* file)
+        # deze los aanroep is gemaakt om caching mogelikj te maken.
         streamfiles = re.findall(
-            "(userbouquet.stream_.*." + soort + ")", linkdata)
+            "(userbouquet.stream_.*." + self.get_soort(content_type) + ")", filedata)
         return streamfiles
 
-    def get_version(self, linkdata, soort):
-        versions = re.findall("userbouquet[.]gemaakt_(.*)." + soort, linkdata)
+    def get_version_from_bouquet(self, filedata, content_type):
+        # ophalen van de versie. zodat je de caching niet hoeft te versen.
+        # niet meer ingebruik, nadat ik een standaard lin heb gebruikt voor caching.
+        versions = re.findall("userbouquet[.]gemaakt_(.*)." + self.get_soort(content_type), filedata)
         return versions[0]
 
-    def get_name(self, linkdata, filename):
+    def get_name(self, filedata, filename):
         try:
-            names = re.findall("^#NAME (.*)", linkdata)
+            # ophalen naam van de filestream bestand
+            names = re.findall("^#NAME (.*)", filedata)
             return names[0]
         except:
             return filename
 
-    def get_items_subfolder(self, linkdata, counter):
-        items = self.get_items(linkdata)
+    def get_items_subfolder(self, filedata, counter):
+        # we halen de data net zo op als in get_items
+        # waar we willen alleen de sub-folder met een bepaalde waarde (de counter)
+        items = self.get_items(filedata)
         for item in items:
             if (item['subfolder'] and item['counter'] == counter):
                 return item
         return
 
-    def get_items(self, linkdata):
-        blokken = re.compile("#SERVICE 1:64:[a-z0-9]*[:0]*").split(linkdata)
+    def get_items(self, filedata):
+        # we delen alle data in alle "blokken".
+        blokken = re.compile("#SERVICE 1:64:[a-z0-9]*[:0]*").split(filedata)
         itemlist = list()
         i = 0
         for blok in blokken:
+            # elk blok wat een sub-folder is begint met "DESCRIPTION"
+            # als dit dus niet het geval is, dan zijn er dus direct items.
             if blok.find("#DESCRIPTION ++") == -1:
                 # we hebben geen subfolder alleen maar NAME of NAME met streams
                 itemlist.append(
                     {'subfolder': False, 'streams': self.get_streams(blok)})
             else:
                 # we hebben een sub folder met streams
+                # we nummeren alle sub-folders, zodat we deze data weer op de zelfde manier kunnen ophalen.
                 i = i + 1
                 itemlist.append({'subfolder': True, 'streams': self.get_streams(
                     blok), 'label': self.get_desciptionfolder(blok), 'counter': str(i)})
         return itemlist
 
     def get_desciptionfolder(self, data):
+        # hier halen we de naam van het blok op.        
         matches = re.findall("#DESCRIPTION ([+][+].*[+][+])", data)
         return matches[0]
 
     def get_streams(self, data):
+        # alle data is gescheiden met :, dus een echte ":" is vertaalt naar "%3a"
+        # we zoeken op deze regular expression, zodat we de naam en url hebben ":" gescheiden.
         streamsandnames = re.findall("([a-z]*%3a.*:.*)", data)
         itemlist = list()
         for streamandname in streamsandnames:
+            # we splitten de regular expression data op : (naam en url)
             streamadres, streamname = streamandname.split(':')
             item = {'label': streamname,
                     'stream': streamadres.replace('%3a', ':')}

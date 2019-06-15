fa96ff499415d67f803cbdb69576a97517ee0337
Bas Magré <Opvolger@gmail.com>
2019-06-15 13:40:46 +0200

    download files

--- stream_checks.py
+++ stream_checks.py
@@ -1,4 +1,4 @@
-import requests, queue, threading, sys, subprocess, csv, os
+import requests, queue, threading, sys, subprocess, csv, os, hashlib
 
 from resources.lib.hanssettings import HansSettings
 from streamcheck.lib.streamobject import StreamObject
@@ -10,6 +10,7 @@ _hanssettings = HansSettings()
 # hier kan je proberen hoeveel threads / workers je tegelijk aan hebt.
 _num_worker_threads = 50
 _timeout = 5
+_access_rights = 0o755
 
 print(_hanssettings)
 
@@ -25,7 +26,7 @@ def worker():
         except requests.ConnectionError:            
             print("Failed to connect - StatusCodeCheck - " + stream.stream_label)
         except:
-            print("Error - StatusCodeCheck - " + stream.stream_label)            
+            print("Error - StatusCodeCheck - " + stream.stream_label)
         # kijken of de stream wat oplevert met FFProbe
         try:
             #print('in: ' + stream.stream_url + ' ('+str(stream.id)+') ')
@@ -40,8 +41,8 @@ def worker():
             #print('in: ' + stream.stream_url + ' ('+str(stream.id)+') ')
             M3u8RedirectOr302(stream, _timeout).run()
             #print('out: ' + stream.stream_url + ' ('+str(stream.id)+') ' + stream.status)
-        except requests.ConnectionError:            
-            print("Failed to connect - M3u8RedirectOr302 - " + stream.stream_label)            
+        except requests.ConnectionError:
+            print("Failed to connect - M3u8RedirectOr302 - " + stream.stream_label)
         except:
             print("Error - M3u8RedirectOr302 - " + stream.stream_label)
         # kijken of de new url stream wat oplevert met FFProbe
@@ -52,7 +53,7 @@ def worker():
         except subprocess.TimeoutExpired:
             print("Timeout - FFProbeCheck - " + stream.stream_label)
         except:
-            print("Error - FFProbeCheck - " + stream.stream_label)            
+            print("Error - FFProbeCheck - " + stream.stream_label)
         # we zijn klaar met deze queue opdracht
         _q.task_done()
 
@@ -88,8 +89,34 @@ github_stream_filenames = _hanssettings.get_stream_files_from_bouguet(stream_lis
 
 # totaal aantal streambestanden welke zijn op te halen van github.
 count_stream_filenames = len(github_stream_filenames)
-
 print('totaal github-files: ' + str(count_stream_filenames))
+
+# version wordt niet meer juist gevuld
+# version = _hanssettings.get_version_from_bouquet(stream_list_github, content_type)
+md5hash = hashlib.md5(stream_list_github.encode())
+version = md5hash.hexdigest()
+
+current_path = os.getcwd()
+
+download_files = True
+check_data_dir = os.path.join(current_path, 'check_data')
+version_dir = os.path.join(check_data_dir, version)
+
+if os.path.isdir(version_dir):
+    download_files = False
+
+if (download_files):
+    if not os.path.isdir(check_data_dir):
+        # maak de data dir aan als deze er nog niet is
+        os.mkdir(check_data_dir, _access_rights)
+    # maak de versie dir aan
+    os.mkdir(version_dir, _access_rights)
+    for filename in github_stream_filenames:
+        datafile = _hanssettings.get_data_from_github_file(filename)
+        file_on_disk = os.path.join(version_dir, filename)
+        with open(file_on_disk, 'wb') as f:
+            f.write(datafile.encode())
+            
 
 # we gaan alle streams per github-file toevoegen aan 1 grote lijst.
 all_streams = list()
@@ -97,13 +124,16 @@ i = 0 # file teller
 j = 0 # stream teller
 for filename in github_stream_filenames:
     i = i + 1
-    datafile = _hanssettings.get_data_from_github_file(filename)
+    file_on_disk = os.path.join(version_dir, filename)
+    openfile = open(file_on_disk,'r', encoding="utf-8")
+    datafile = openfile.read()
     name = _hanssettings.get_name(datafile, filename)
     print(str(i) + ': ' + name)
     streams_datafile = _hanssettings.get_streams(datafile)
     for stream in streams_datafile:
         j = j + 1
         all_streams.append(StreamObject(j, filename, name, stream['label'], stream['url'], stream['header']))
+    openfile.close()
     # voor testen even met 4 files
     # if (i == 1):
     #     break
@@ -111,26 +141,22 @@ for filename in github_stream_filenames:
 sum_run0 = sum(st.status != 'OK' for st in all_streams)
 print('Run0:' + str(sum_run0))
 print('---')
-
 _q = queue.Queue()
 loop()
-sum_run1 = sum(st.status != 'OK' for st in all_streams)
-print('Run1:' + str(sum_run1))
-print('---')
 
-_q = queue.Queue()
-_timeout = 10
-loop()
-sum_run2 = sum(st.status != 'OK' for st in all_streams)
-print('Run2:' + str(sum_run2))
-print('---')
+# sum_run1 = sum(st.status != 'OK' for st in all_streams)
+# print('Run1:' + str(sum_run1))
+# print('---')
+# _q = queue.Queue()
+# _timeout = 10
+# loop()
 
-_q = queue.Queue()
-_timeout = 30
-loop()
-sum_run3 = sum(st.status != 'OK' for st in all_streams)
-print('Run3:' + str(sum_run3))
-print('---')
+# sum_run2 = sum(st.status != 'OK' for st in all_streams)
+# print('Run2:' + str(sum_run2))
+# print('---')
+# _q = queue.Queue()
+# _timeout = 30
+# loop()
 
 print('done queues')
 
@@ -170,12 +196,12 @@ print('---')
 print('Run0')
 print(sum_run0)
 print('---')
-print('Run1')
-print(sum_run1)
-print('---')
-print('Run2')
-print(sum_run2)
-print('---')
-print('Run3')
-print(sum_run3)
-print('---')
+# print('Run1')
+# print(sum_run1)
+# print('---')
+# print('Run2')
+# print(sum_run2)
+# print('---')
+# print('Run3')
+# print(sum_run3)
+# print('---')

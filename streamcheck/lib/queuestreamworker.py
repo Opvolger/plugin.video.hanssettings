ca5c310a5f466f3aa97634a10c05150d2a928d27
Bas Magré <Opvolger@gmail.com>
2019-06-16 03:36:30 +0200

    timeout met threading opgelost.

--- streamcheck/lib/queuestreamworker.py
+++ streamcheck/lib/queuestreamworker.py
@@ -1,8 +1,51 @@
-import requests, subprocess, threading, sys
+import requests, subprocess, threading, sys, time, ctypes
 
 from streamcheck.lib.checks.ffprobecheck import FFProbeCheck
 from streamcheck.lib.checks.statuscodecheck import StatusCodeCheck
 from streamcheck.lib.checks.m3u8redirector302 import M3u8RedirectOr302
+
+class CheckThread(threading.Thread): 
+    def __init__(self, worker_id, stream, queue_logging, timeout): 
+        threading.Thread.__init__(self)         
+        self.timeout =  timeout
+        self.stream = stream
+        self.worker_id = worker_id
+        self.queue_logging = queue_logging
+
+    def run_check(self, check):
+        try:
+            # self.queue_logging.put(str(self.worker_id) + " " + check.stream.debug_format("Start"))
+            check.run()
+        except requests.ConnectionError:                
+            self.queue_logging.put(str(self.worker_id) + " " + check.stream.debug_format("Failed to connect - " + check.__class__.__name__))
+        except subprocess.TimeoutExpired:                
+            self.queue_logging.put(str(self.worker_id) + " " + check.stream.debug_format( "Timeout - " + check.__class__.__name__))
+        except:                
+            self.queue_logging.put(str(self.worker_id) + " " + check.stream.debug_format("Error - " + check.__class__.__name__))
+
+    def run(self): 
+  
+            # target function of the thread class 
+            try: 
+                # We proberen een http status-code te bepalen
+                check = StatusCodeCheck(self.stream, self.timeout)
+                self.run_check(check)
+                
+                # We doen een FFProbe controle
+                check = FFProbeCheck(self.stream, self.timeout)
+                self.run_check(check)
+
+                # We kijken of er een redirect is in de M3U8-file
+                check = M3u8RedirectOr302(self.stream, self.timeout)
+                self.run_check(check)
+                
+                # we controleren de redirect, misschien werkt deze wel.
+                check = FFProbeCheck(self.stream, self.timeout)
+                check.set_url(self.stream.new_stream_url)
+                self.run_check(check)                
+            except:
+                print(self.stream.debug_format(" stuk gelopen"))
+
 
 # Deze class haalt opdrachten van de queue af en gaat controles draaien op de streams
 class QueueStreamWorker():
@@ -14,20 +57,14 @@ class QueueStreamWorker():
         self.timeout = timeout
         self.workers_aantal = workers_aantal
 
-    def quit_function(self, fn_name):
+    def quit_function(self, fn_name, check):
         # print to stderr, unbuffered in Python 2.
         self.queue_logging.put('{0} took too long'.format(fn_name))
+        # ondanks de "kill" bij timeout, blijft de ffprobe "hangen", tot het process is gekilled.
+        # daarom deze timer, dat de werker wel weer vrij komt en we niet wachten op de kill, maar deze op de achtergrond dus nog door gaat
+        # dit is ook de reden dat het einde soms wat lang duurt. Er zit dan dus nog een process te wachten op zijn kill :)
+        del check        
         sys.stderr.flush() # Python 3 stderr is likely buffered.
-
-    def run_check(self, check):
-        try:
-            check.run()
-        except requests.ConnectionError:                
-            self.queue_logging.put(check.stream.debug_format("Failed to connect - " + check.__class__.__name__))
-        except subprocess.TimeoutExpired:                
-            self.queue_logging.put(check.stream.debug_format( "Timeout - " + check.__class__.__name__))
-        except:                
-            self.queue_logging.put(check.stream.debug_format("Error - " + check.__class__.__name__))
 
     def start(self):
         while True:
@@ -35,32 +72,10 @@ class QueueStreamWorker():
             if stream is None:
                 self.queue_logging.put("QueueStreamWorker nummer (" + str(self.worker_id) + ") is gestopt: maar er zijn er wel " + str(self.workers_aantal))
                 # De queue is leeg.
-                break            
-            timer = threading.Timer(self.timeout, self.quit_function, args=[stream.debug_format()])
-            timer.start()
-            check = None
-            try:
-                # We proberen een http status-code te bepalen
-                check = StatusCodeCheck(stream, self.timeout)
-                self.run_check(check)
-                
-                # We doen een FFProbe controle
-                check = FFProbeCheck(stream, self.timeout)
-                self.run_check(check)
-
-                # We kijken of er een redirect is in de M3U8-file
-                check = M3u8RedirectOr302(stream, self.timeout)
-                self.run_check(check)
-                
-                # we controleren de redirect, misschien werkt deze wel.
-                check = FFProbeCheck(stream, self.timeout)
-                check.set_url(stream.new_stream_url)
-                self.run_check(check)
-            finally:
-                timer.cancel()
-                # ondanks de "kill" bij timeout, blijft de ffprobe "hangen", tot het process is gekilled.
-                # daarom deze timer, dat de werker wel weer vrij komt en we niet wachten op de kill, maar deze op de achtergrond dus nog door gaat
-                # dit is ook de reden dat het einde soms wat lang duurt. Er zit dan dus nog een process te wachten op zijn kill :)
-                del check
+                break
+            # we werken met een CheckThread, zodat we naar de timeout de nek om kunnen draaien van dit ding.
+            t1 = CheckThread(self.worker_id, stream, self.queue_logging, self.timeout) 
+            t1.start()
+            t1.join(self.timeout)
             # we zijn klaar met deze queue opdracht
             self.queue.task_done()
